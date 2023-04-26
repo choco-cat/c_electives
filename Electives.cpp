7bf77e07237dbabc8b2ad56a2f7161c38211d66d
Natalia Panfilenka <choco-cat@mail.ru>
2023-04-26 18:15:59 +0300

    path electives

---
+++ Electives.cpp
@@ -0,0 +1,43 @@
+#include "Prototypes and Classes.h"
+
+// преписываю каждому факультативу своего преподавателя
+
+FILE phisic, math, english, database, * programming;
+
+//errno_t err;
+
+/*int fileElectives()
+{
+    err = fopen_s(&phisic, "DataOfStudent.bin", "w+");
+    if (!data)
+    {
+        printf("Ошибка при работе с файлом (его создании или открытии) .\n");
+        return -2;
+    }
+    err = fopen_s(&data, "DataOfStudent.bin", "a+");
+    if (!data)
+    {
+        printf("Ошибка при работе с файлом (его создании или открытии) .\n");
+        return -2;
+    }
+    err = fopen_s(&data, "DataOfStudent.bin", "a+");
+    if (!data)
+    {
+        printf("Ошибка при работе с файлом (его создании или открытии) .\n");
+        return -2;
+    }
+    err = fopen_s(&data, "DataOfStudent.bin", "a+");
+    if (!data)
+    {
+        printf("Ошибка при работе с файлом (его создании или открытии) .\n");
+        return -2;
+    }
+    err = fopen_s(&data, "DataOfStudent.bin", "a+");
+    if (!data)
+    {
+        printf("Ошибка при работе с файлом (его создании или открытии) .\n");
+        return -2;
+    }
+
+    return 0;
+}*/
